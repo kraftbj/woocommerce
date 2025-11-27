950f76fb39e04d6516347b43ddc6f33ca3a61af2
Mayisha <mayisha.alam@automattic.com>
2025-11-27 22:17:57 +0600

    show notice when currency is not supported

--- plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-notices.php
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-notices.php
@@ -32,6 +32,13 @@ class WC_Gateway_Paypal_Notices {
 	const PAYPAL_ACCOUNT_RESTRICTED_NOTICE = 'paypal_account_restricted';
 
 	/**
+	 * The name of the notice for PayPal unsupported currency.
+	 *
+	 * @var string
+	 */
+	private const PAYPAL_UNSUPPORTED_CURRENCY_NOTICE = 'paypal_unsupported_currency';
+
+	/**
 	 * The PayPal gateway instance.
 	 *
 	 * @var WC_Gateway_Paypal
@@ -73,6 +80,7 @@ class WC_Gateway_Paypal_Notices {
 
 		$this->add_paypal_migration_notice();
 		$this->add_paypal_account_restricted_notice();
+		$this->add_paypal_unsupported_currency_notice();
 	}
 
 	/**
@@ -89,8 +97,7 @@ class WC_Gateway_Paypal_Notices {
 			return;
 		}
 
-		$this->add_paypal_migration_notice();
-		$this->add_paypal_account_restricted_notice();
+		$this->add_paypal_notices();
 	}
 
 	/**
@@ -149,6 +156,39 @@ class WC_Gateway_Paypal_Notices {
 		$notice_html = '<div class="notice notice-error is-dismissible">'
 			. '<a class="woocommerce-message-close notice-dismiss" style="text-decoration: none;" href="' . esc_url( $dismiss_url ) . '"></a>'
 			. '<p><strong>' . esc_html__( 'PayPal Account Restricted', 'woocommerce' ) . '</strong></p>'
+			. '<p>' . $message . '</p>'
+			. '</div>';
+
+		echo wp_kses_post( $notice_html );
+	}
+
+	/**
+	 * Add notice warning when PayPal does not support the store's currency.
+	 *
+	 * @return void
+	 */
+	private function add_paypal_unsupported_currency_notice() {
+		$currency = get_woocommerce_currency();
+
+		// Skip if the currency is supported by PayPal.
+		if ( in_array( $currency, WC_Gateway_Paypal_Constants::SUPPORTED_CURRENCIES, true ) ) {
+			return;
+		}
+
+		// Skip if the notice has been dismissed.
+		if ( $this->is_notice_dismissed( self::PAYPAL_UNSUPPORTED_CURRENCY_NOTICE ) ) {
+			return;
+		}
+
+		$dismiss_url = $this->get_dismiss_url( self::PAYPAL_UNSUPPORTED_CURRENCY_NOTICE );
+		$message = sprintf(
+			/* translators: %s: Currency code */
+			esc_html__( 'PayPal Standard does not support your store currency (%s).', 'woocommerce' ),
+			$currency
+		);
+
+		$notice_html = '<div class="notice notice-error is-dismissible">'
+			. '<a class="woocommerce-message-close notice-dismiss" style="text-decoration: none;" href="' . esc_url( $dismiss_url ) . '"></a>'
 			. '<p>' . $message . '</p>'
 			. '</div>';
 
